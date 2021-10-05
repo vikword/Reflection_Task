c9221539652b2e2d54573ebf80d4b2ce6f05541a
user <user@WIN-RA2CDU1E29O>
2021-10-05 22:03:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection_Task
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(TestClass);
+            var instance = Activator.CreateInstance(type) as TestClass;
+
+
+            #region Получаем данные о типе
+
+            ListVariosStats(instance);
+            ListMethods(instance);
+            ListFields(instance);
+            ListProps(instance);
+            ListConstructors(instance);
+
+            #endregion
+
+            #region Обращение к закрытым членам
+
+            Console.WriteLine(new string('-', 60));
+
+            MethodInfo method3 = type.GetMethod("Method3", BindingFlags.Instance | BindingFlags.NonPublic);
+            method3.Invoke(instance, new object[] { "Hello", " World!" });
+
+            Console.WriteLine(new string('-', 30));
+
+            FieldInfo mystring = type.GetField("_mystring", BindingFlags.Instance | BindingFlags.NonPublic);
+            mystring.SetValue(instance, "Привет Мир!");
+
+            Console.WriteLine(instance.MyString);
+
+            Console.WriteLine(new string('-', 30));
+
+            FieldInfo myint = type.GetField("_myint", BindingFlags.Instance | BindingFlags.NonPublic);
+            myint.SetValue(instance, 10);
+
+            Console.WriteLine(instance.MyInt);
+
+            #endregion
+        }
+        private static void ListVariosStats(TestClass instance)
+        {
+            Console.WriteLine(new string('-', 30) + " Информация о MyClass" + "\n");
+            Type t = instance.GetType();
+
+            Console.WriteLine($"Полное имя:             {t.FullName}");
+            Console.WriteLine($"Базовый класс:          {t.BaseType}");
+            Console.WriteLine($"Абстрактный:            {t.IsAbstract}");
+            Console.WriteLine($"Это COM Объект:         {t.IsCOMObject}");
+            Console.WriteLine($"Запрещено Наслелование: {t.IsSealed}");
+            Console.WriteLine($"Это Class:              {t.IsClass}");
+        }
+        private static void ListMethods(TestClass instance)
+        {
+            Console.WriteLine(new string('-', 30) + " Методы класса MyClass" + "\n");
+
+            Type t = instance.GetType();
+            MethodInfo[] mi = t.GetMethods(
+                BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.Public |
+                BindingFlags.NonPublic |
+                BindingFlags.DeclaredOnly);
+
+            foreach (MethodInfo m in mi)
+            {
+                Console.WriteLine($"Method: {m.Name}");
+            }
+        }
+        private static void ListFields(TestClass instance)
+        {
+            Console.WriteLine(new string('-', 30) + " Поля класса MyClass" + "\n");
+
+            Type t = instance.GetType();
+            FieldInfo[] fi = t.GetFields(
+                BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.Public |
+                BindingFlags.NonPublic);
+
+            foreach (FieldInfo f in fi)
+            {
+                Console.WriteLine($"Field: {f.Name}");
+            }
+        }
+        private static void ListProps(TestClass instance)
+        {
+            Console.WriteLine(new string('-', 30) + " Свойства класса MyClass" + "\n");
+
+            Type t = instance.GetType();
+            PropertyInfo[] pi = t.GetProperties();
+
+            foreach (PropertyInfo p in pi)
+            {
+                Console.WriteLine($"Property: {p.Name}");
+            }
+        }
+        private static void ListConstructors(TestClass instance)
+        {
+            Console.WriteLine(new string('-', 30) + " Конструкторы класса MyClass" + "\n");
+
+            Type t = instance.GetType();
+            ConstructorInfo[] ci = t.GetConstructors();
+
+            foreach (ConstructorInfo c in ci)
+            {
+                Console.WriteLine($"Constructor: {c.Name}");
+            }
+        }
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Reflection_Task
+{
+    class TestClass
+    {
+        private int _myint;
+        private string _mystring = "Hello World!";
+
+        public TestClass()
+        {
+        }
+        public TestClass(int myint)
+        {
+            _myint = myint;
+        }
+        public int MyInt
+        {
+            get { return _myint; }
+            set { _myint = value; }
+        }
+        public string MyString
+        {
+            get { return _mystring; }
+        }
+        public void Method1()
+        {
+        }
+        protected void Method2()
+        {
+        }
+        private void Method3(string str, string str2)
+        {
+            Console.WriteLine("Private метод");
+            Console.WriteLine(str + str2);
+        }
+    }
+}
